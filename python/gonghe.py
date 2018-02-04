97c30d724e772524b817efde6a2b6cbaf940047f
Tzeng Yuxio <tzengyuxio@gmail.com>
2018-02-05 05:16:32 +0800

    feature: 找共和元年立春

--- python/gonghe.py
+++ python/gonghe.py
@@ -20,6 +20,12 @@ def ganzhi_of_day(jd):
     return (int(floor(jd+.5)) - 11) % 60
 
 
+# year = 0 為不合法
+def ganzhi_of_year(year):
+    year = year if year > 0 else year + 1
+    return (year-4) % 60
+
+
 # 往回尋找冬至朔旦甲子(dzsdjz)
 # tz 時區(小時)
 # gz 干支, -1 表示不指定
@@ -38,16 +44,38 @@ def previous_dzsdjz(start_date, tz=0, gz=-1, delta=0.333333):
             continue
         if abs(sd - md) > delta:
             continue
-        daygz = ganzhi_of_day(ephem.julian_date(d)) # 日干支
+        daygz = ganzhi_of_day(ephem.julian_date(d))  # 日干支
         if gz != -1 and daygz != gz:
             continue
-        print(sd_ymd, ganzhi(daygz))
+        yeargz = ganzhi_of_year(sd_ymd[0])           # 年干支
+        print(sd_ymd, ganzhi(yeargz), '年,', ganzhi(daygz), '日')
         print(sd, ephem.julian_date(sd))
         print(md, ephem.julian_date(md))
         break
     return d
 
 
-d = start_date
-for i in range(1):
-    d = previous_dzsdjz(d, gz=0)
+def lichun_of_year(year):
+    d = ephem.Date((year, 1, 1))
+    eq = ephem.next_spring_equinox(d)
+    sol = ephem.previous_winter_solstice(d)
+    lichun = ephem.Date((eq + sol) / 2)
+    lichun_ymd = lichun.tuple()[:3]
+    daygz = ganzhi_of_day(ephem.julian_date(lichun))
+    yeargz = ganzhi_of_year(lichun_ymd[0])
+    print(lichun_ymd, ganzhi(yeargz), '年,', ganzhi(daygz), '日')
+    print(lichun, ephem.julian_date(lichun))
+
+def test_of_zero_year():
+    d1 = ephem.Date((1, 1, 1))
+    d2 = ephem.Date(d1 - 1.0)
+    print(d2.tuple()[:3])
+
+# 往回尋找冬至朔旦甲子
+# d = start_date
+# for i in range(1):
+#     d = previous_dzsdjz(d, gz=0)
+
+for i in range(-840, -844, -1):
+    lichun_of_year(i)
+
